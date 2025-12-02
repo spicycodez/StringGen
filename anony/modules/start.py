8eeae2c13ad5bc87483c3f77dcb55e775a41e391
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2025-12-02 23:21:39 +0530

    Update start.py

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -8,7 +8,7 @@ async def f_start(_, m: types.Message):
      await m.reply_photo(
           photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
           has_spoiler=True,
-          caption=f"ʜᴇʏ {m.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+          caption=f"ʜᴇʏ {m.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {app.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
           reply_markup=keyboard,
      )
      await db.add_user(m.from_user.id)
