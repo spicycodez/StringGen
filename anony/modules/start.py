9284ba4caf5a6c7f1f8243594df90868ef291792
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2025-12-02 23:16:36 +0530

    Update start.py

--- anony/modules/start.py
+++ anony/modules/start.py
@@ -5,10 +5,10 @@ from anony import app, buttons, db
 
 @app.on_message(filters.command(["start"]) & filters.private)
 async def f_start(_, m: types.Message):
-     await message.reply_photo(
-        photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
-         has_spoiler = True,
-       caption=f"ʜᴇʏ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
-        reply_markup=keyboard,
-    )
-    await db.add_user(m.from_user.id)
+     await m.reply_photo(
+          photo="https://graph.org/file/0c425a271a2caae5d62dc.jpg",
+          has_spoiler=True,
+          caption=f"ʜᴇʏ {m.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\nAɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.",
+          reply_markup=keyboard,
+     )
+     await db.add_user(m.from_user.id)
